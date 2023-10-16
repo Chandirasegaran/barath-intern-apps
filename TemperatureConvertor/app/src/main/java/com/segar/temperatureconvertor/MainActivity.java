75441b975f61696054c28c5a84ba210e6a600dfb
Chandirasegaran <chandirasegaransegar@gmail.com>
2023-10-16 20:43:22 +0530

    Done Temp conv App

--- TemperatureConvertor/app/src/main/java/com/segar/temperatureconvertor/MainActivity.java
+++ TemperatureConvertor/app/src/main/java/com/segar/temperatureconvertor/MainActivity.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 Button ctf;
 Button ftc;
-//TextView display;
+TextView display;
 EditText textbox;
 
     @Override
@@ -21,7 +21,7 @@ EditText textbox;
         setContentView(R.layout.activity_main);
         ctf=findViewById(R.id.ctf);
         ftc=findViewById(R.id.ftc);
-//        display=findViewById(R.id.display);
+        display=findViewById(R.id.display);
         textbox=findViewById(R.id.tempvalue);
 
 
@@ -29,8 +29,30 @@ EditText textbox;
             @Override
             public void onClick(View view) {
                 String val=textbox.getText().toString();
-//                display.setText(val);
-                Toast.makeText(getApplicationContext(),val,Toast.LENGTH_SHORT).show();
+                if (val.isEmpty()){
+                    display.setText("Enter the Temperature.");
+                }
+                else{
+                    double c = Double.parseDouble(val);
+                    double res= (c*(1.8))+32;
+                    display.setText("Fahrenheit:  "+String.format("%.2f",res)+"°F");
+                }
+
+
+            }
+        });
+        ftc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String val=textbox.getText().toString();
+                if (val.isEmpty()){
+                    display.setText("Enter the Temperature.");
+                }
+                else {
+                    double f = Double.parseDouble(val);
+                    double res = ((f - 32) * (5.0 / 9.0));
+                    display.setText("Celcius:  " + String.format("%.2f", res) + "°C");
+                }
             }
         });
     }
